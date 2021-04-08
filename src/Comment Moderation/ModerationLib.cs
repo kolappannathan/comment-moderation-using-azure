48041579b187d707690cd2df6e88c1150ab04624
Kolappan Nathan <kolappannathan@users.noreply.github.com>
2021-04-08 12:59:50 +0530

    Adding Term list support

--- src/Comment Moderation/ModerationLib.cs
+++ src/Comment Moderation/ModerationLib.cs
@@ -1,28 +1,42 @@
 ﻿using Microsoft.Azure.CognitiveServices.ContentModerator;
 using Microsoft.Azure.CognitiveServices.ContentModerator.Models;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading;
 
 namespace Comment_Moderation
 {
     public class ModerationLib
     {
+        #region Credentials
+
         private static readonly string SubscriptionKey = "SAMPLE_KEY";
         private static readonly string Endpoint = "SAMPLE_ENDPOINT";
 
-        public void ModerateText(string text)
+        #endregion Credentials
+
+        private ContentModeratorClient client;
+
+        public ModerationLib()
         {
-            var azClient = Authenticate();
-            var azResult = CallAzureModerator(azClient, text);
+            client = Authenticate();
+        }
 
-            // TODO:
-            // Check if the text contains a blocklisted word using List
+        #region Moderating Text
 
+        public void ModerateText(string text, string listId)
+        {
+            var azResult = CallAzureModerator(text, listId);
             if (azResult.PII != null && (azResult.PII.Email.Count > 0 || azResult.PII.Phone.Count > 0))
             {
                 // Don't post the comment and inform the user
                 Console.WriteLine("Contains Personal Data");
+            }
+            else if (azResult.Terms != null)
+            {
+                Console.WriteLine("Contains Blocked words from the list");
             }
             else if (azResult.Classification.ReviewRecommended == true)
             {
@@ -55,11 +69,55 @@ namespace Comment_Moderation
             return client;
         }
 
-        private Screen CallAzureModerator(ContentModeratorClient client, string text)
+        private Screen CallAzureModerator(string text, string listId)
         {
             var textBytes = Encoding.UTF8.GetBytes(text);
             using var stream = new MemoryStream(textBytes);
-            return client.TextModeration.ScreenText("text/plain", stream, language: "eng", autocorrect: false, pII: true, listId:null, true);
+            return client.TextModeration.ScreenText("text/plain", stream, language: "eng", autocorrect: false, pII: true, listId:listId, classify: true);
         }
+
+        #endregion Moderating Text
+
+        #region Managing Lists
+
+        public string CreateTermList(string listName, string listDesc)
+        {
+            var body = new Body(listName, listDesc);
+            var list = client.ListManagementTermLists.Create("application/json", body);
+            var listId = list.Id.Value.ToString();
+            return listId;
+        }
+
+        public void AddToTermsList(string listId, List<string> terms, int throttleRate = 3000, string lang = "eng")
+        {
+            foreach(var term in terms)
+            {
+                var result = client.ListManagementTerm.AddTerm(listId, term, lang);
+                Thread.Sleep(throttleRate);
+            }
+        }
+
+        public List<string> GetAllTerms(string listId, string lang = "eng")
+        {
+            var termsData = client.ListManagementTerm.GetAllTerms(listId, lang).Data;
+            List<string> termsList = new List<string>();
+            foreach (var term in termsData.Terms)
+            {
+                termsList.Add(term.Term);
+            }
+            return termsList;
+        }
+
+        public void DeleteTerm(string listId, string term, string lang = "eng")
+            => client.ListManagementTerm.DeleteTerm(listId, term, lang);
+
+        public void DeleteAllTerms(string listId, string lang = "eng")
+            => client.ListManagementTerm.DeleteAllTerms(listId, lang);
+
+        public void DeleteTermList(string listId)
+            => client.ListManagementTermLists.Delete(listId);
+
+        #endregion Managing Lists
+
     }
 }
--- src/Comment Moderation/Program.cs
+++ src/Comment Moderation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Comment_Moderation
 {
@@ -8,12 +9,148 @@ namespace Comment_Moderation
         {
             do
             {
-                Console.WriteLine("Enter the text to be moderated: ");
-                var text = Console.ReadLine();
-                var moderation = new ModerationLib();
-                moderation.ModerateText(text);
+                Console.WriteLine("1: Manage Terms List");
+                Console.WriteLine("2: Moderate Text");
+                Console.WriteLine("Enter your choice: ");
+                var choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        HandleListManagement();
+                        break;
+                    case "2":
+                        HandleModerateText();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                };
                 Console.WriteLine("");
+                Console.ReadLine();
             } while (true);
         }
+
+        public static void HandleModerateText()
+        {
+            Console.WriteLine("Is there a moderation list: Y/N");
+            var choice = Console.ReadKey().KeyChar;
+            string listId = null;
+            Console.ReadLine();
+            if (choice == 'Y' || choice == 'y')
+            {
+                Console.WriteLine("Enter List Id");
+                listId = Console.ReadLine();
+            }
+            Console.WriteLine("Enter the text to be moderated: ");
+            var text = Console.ReadLine();
+            var moderationLib = new ModerationLib();
+            moderationLib.ModerateText(text, listId);
+        }
+
+        #region List Management
+
+        public static void HandleListManagement()
+        {
+            Console.WriteLine("1: Create Terms List");
+            Console.WriteLine("2: Add terms to a Term List");
+            Console.WriteLine("3: List all Terms in a List");
+            Console.WriteLine("4: Remove a term from List");
+            Console.WriteLine("5: Empty a List");
+            Console.WriteLine("6: Delete a List");
+            var choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    CreateTermList();
+                    break;
+                case "2":
+                    AddTermsToList();
+                    break;
+                case "3":
+                    ListAllTerms();
+                    break;
+                case "4":
+                    RemoveOneTerm();
+                    break;
+                case "5":
+                    RemoveAllTerms();
+                    break;
+                case "6":
+                    DeleteList();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Choice");
+                    break;
+            };
+        }
+
+        public static void CreateTermList()
+        {
+            Console.WriteLine("Enter list name");
+            var listName = Console.ReadLine();
+            Console.WriteLine("Enter list description");
+            var listDesc = Console.ReadLine();
+            var moderationLib = new ModerationLib();
+            var listId = moderationLib.CreateTermList(listName, listDesc);
+            Console.WriteLine($"List created with id:{listId}");
+        }
+
+        public static void AddTermsToList()
+        {
+            Console.WriteLine("Enter number of terms to add");
+            var no_of_terms = Convert.ToInt32(Console.ReadLine());
+            List<string> terms_List = new List<string>();
+            Console.WriteLine("Enter the terms");
+            for (var i=0; i<no_of_terms; i++)
+            {
+                terms_List.Add(Console.ReadLine());
+            }
+            Console.WriteLine("Enter List Id");
+            var listId = Console.ReadLine();
+            var moderationLib = new ModerationLib();
+            moderationLib.AddToTermsList(listId, terms_List);
+            Console.WriteLine("Terms added successfully");
+        }
+
+        public static void ListAllTerms()
+        {
+            Console.WriteLine("Enter List Id");
+            var listId = Console.ReadLine();
+            var moderationLib = new ModerationLib();
+            var terms = moderationLib.GetAllTerms(listId);
+            var csv = string.Join(",", terms);
+            Console.WriteLine($"Terms are: {csv}");
+        }
+
+        public static void RemoveOneTerm()
+        {
+            Console.WriteLine("Enter List Id");
+            var listId = Console.ReadLine();
+            Console.WriteLine("Enter the term to be removed");
+            var term = Console.ReadLine();
+            var moderationLib = new ModerationLib();
+            moderationLib.DeleteTerm(listId, term);
+            Console.WriteLine("Term is removed");
+        }
+
+        public static void RemoveAllTerms()
+        {
+            Console.WriteLine("Enter List Id");
+            var listId = Console.ReadLine();
+            var moderationLib = new ModerationLib();
+            moderationLib.DeleteAllTerms(listId);
+            Console.WriteLine("All terms are removed from the list");
+        }
+
+        public static void DeleteList()
+        {
+            Console.WriteLine("Enter List Id");
+            var listId = Console.ReadLine();
+            var moderationLib = new ModerationLib();
+            moderationLib.DeleteTermList(listId);
+            Console.WriteLine("Terms list deleted");
+        }
+
+        #endregion List Management
     }
 }
